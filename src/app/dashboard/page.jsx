8a643174f5f03a54a3566be5e9cd74e67a1535b8
Ksawert560 <56079058+Ksawert560@users.noreply.github.com>
2025-03-31 19:02:04 +0200

    Ahh yes dashboard and profile pics

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,12 +1,48 @@
+'use client'
 import React from "react";
 import Nav from "../components/Nav";
 import styles from "../../app/main.css";
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
 
 function Dashboard(){
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [userAvatar, setUserAvatar] = useState(null);
+
+  useEffect(() => {
+    const jwtSession = localStorage.getItem('JWT_REFRESH');
+    const savedAvatar = localStorage.getItem('userAvatar')
+
+    if (!jwtSession) {
+      router.push('/');
+    } else {
+      setIsAuthenticated(true);
+    }
+    setIsLoading(false);
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!isAuthenticated) {
+    return null; // Or a message like "Redirecting..."
+  }
+  if (localStorage.getItem("userAvatar")){
+    console.log("Avatar exists");
+  }
+  const avatar = localStorage.getItem("userAvatar");
     return (
     <main className={styles.page}>
-        dashboard
+        <section className="sideBar">
+            {avatar && (
+                <div className="userAvatar" dangerouslySetInnerHTML={{ __html: avatar }} />
+            )}
+        </section>
     </main>
     )
 }
