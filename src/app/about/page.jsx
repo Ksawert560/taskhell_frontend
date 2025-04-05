f7f568dbcbe35dd9d8d29f15b5349d46e0f11f5a
mskubarc <milosz.skubarczewski@outlook.com>
2025-04-06 00:42:06 +0200

    Changes to the 'about' page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -14,15 +14,29 @@ function About(){
     <main className={styles.page}>
         <Nav />
         <section className="aboutSection">
-            <h2>About project</h2>
+            <h2>About the project</h2>
             <p> 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor, augue ac suscipit sodales, lectus turpis consequat nibh, eu faucibus odio massa eget lacus. Praesent risus magna, vestibulum quis scelerisque vel, iaculis in enim. Sed tristique nulla fringilla, imperdiet purus sit amet, pretium orci. Pellentesque ac ante laoreet, iaculis turpis vel, pharetra diam. Donec elementum, dolor eget porta fermentum, purus enim suscipit eros, eget commodo ex justo et odio. Nunc lacinia, enim et interdum venenatis, elit nulla vulputate tortor, non sollicitudin urna tellus sed ligula. Cras ex felis, scelerisque et justo ac, malesuada interdum arcu. In pulvinar augue ac euismod porttitor. Integer id malesuada ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam non risus consequat, ultrices sapien quis, sollicitudin elit. Aliquam aliquet velit sapien, sit amet blandit mi aliquam a. Aenean sed feugiat elit, nec malesuada ligula.
-            Duis dapibus ipsum non ipsum venenatis malesuada. Fusce ultrices leo nisi, eu facilisis nulla blandit quis. Duis sed purus mattis, pharetra felis et, consequat nibh. Vivamus blandit nisl dignissim interdum ornare. Sed dictum magna quis massa dictum tincidunt. Morbi sed maximus justo, id scelerisque arcu. Vivamus et dictum neque. In viverra enim quis facilisis ultricies. Aenean gravida viverra augue eget lobortis. Pellentesque eleifend sagittis consequat. Donec eget mattis mi, vitae euismod risus. Quisque imperdiet a eros dapibus lacinia. Nulla vestibulum commodo nibh vitae lobortis. Integer aliquam venenatis metus, eget pellentesque ante ultricies eu. Nunc sagittis mauris vel porttitor pellentesque. Ut a feugiat diam. 
+                Taskhell is an idea that sprouted up between the three of us on a <s>brisk and sunny</s> rainy and depressing morning. 
+                <span className="lowOpacity">[we're living in an Eastern block country, vitamin D doesn't exist here, just like our hopes and dreams]</span>
             </p>
-            <h2>About authors</h2>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor, augue ac suscipit sodales, lectus turpis consequat nibh, eu faucibus odio massa eget lacus. Praesent risus magna, vestibulum quis scelerisque vel, iaculis in enim. Sed tristique nulla fringilla, imperdiet purus sit amet, pretium orci. Pellentesque ac ante laoreet, iaculis turpis vel, pharetra diam. Donec elementum, dolor eget porta fermentum, purus enim suscipit eros, eget commodo ex justo et odio. Nunc lacinia, enim et interdum venenatis, elit nulla vulputate tortor, non sollicitudin urna tellus sed ligula. Cras ex felis, scelerisque et justo ac, malesuada interdum arcu. In pulvinar augue ac euismod porttitor. Integer id malesuada ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam non risus consequat, ultrices sapien quis, sollicitudin elit. Aliquam aliquet velit sapien, sit amet blandit mi aliquam a. Aenean sed feugiat elit, nec malesuada ligula.
-            Duis dapibus ipsum non ipsum venenatis malesuada. Fusce ultrices leo nisi, eu facilisis nulla blandit quis. Duis sed purus mattis, pharetra felis et, consequat nibh. Vivamus blandit nisl dignissim interdum ornare. Sed dictum magna quis massa dictum tincidunt. Morbi sed maximus justo, id scelerisque arcu. Vivamus et dictum neque. In viverra enim quis facilisis ultricies. Aenean gravida viverra augue eget lobortis. Pellentesque eleifend sagittis consequat. Donec eget mattis mi, vitae euismod risus. Quisque imperdiet a eros dapibus lacinia. Nulla vestibulum commodo nibh vitae lobortis. Integer aliquam venenatis metus, eget pellentesque ante ultricies eu. Nunc sagittis mauris vel porttitor pellentesque. Ut a feugiat diam. 
+                You want an easy and intuitive way to manage your tasks? How about you manage your expectations. Our mission was to create an annoying experience for our users that prevents them from
+                reaching their daily goals. Perhaps you'd like to add a new item to your to-do list? Well tough luck, get ready to go outside and touch grass first before you can even begin 
+                thinking about writing down whatever it is you actually have to do. <br></br>
+            </p>
+            <p>
+                This website was developed for an university course final assignment. The frontend was built using Next.js and the backend was handled with PHP, which allowed us to remain
+                compliant with the project requirements, while still leaving some room to introduce some complexity.
+            </p>
+            <h2>About the authors</h2>
+            <p>
+                <Link href="https://github.com/Ksawert560"> Ksawery Kucz </Link> - responsible for creating the app's smooth and modern design, as well as building the frontend.
+            </p>
+            <p>
+                <Link href="https://github.com/TestkaJakub"> Jakub Testka </Link> - expertly took care of the backend, decided to elegantly throw XAMPP out of the window in favor of Docker, God bless that decision.
+            </p>
+            <p>
+                <Link href="https://github.com/mskubarc"> Miłosz Skubarczewski </Link> - took part in the design process, handled typewriting and the project's presentation.
             </p>
         </section>
     </main>
